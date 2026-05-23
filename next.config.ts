3576b98bc204b0b6b5b584d85c6e2244111b936c
baesw <hahabsw@gmail.com>
2026-05-23 16:57:18 +0900

    Remove temporary demo migration endpoint

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,3 @@
 import { createNextConfig } from "@nexpress/app/config/next-config";
-import type { NextConfig } from "next";
 
-const config = createNextConfig();
-
-export default {
-  ...config,
-  outputFileTracingIncludes: {
-    ...(config.outputFileTracingIncludes ?? {}),
-    "/api/internal/demo-migrate": ["./drizzle/**/*"],
-  },
-} satisfies NextConfig;
+export default createNextConfig();
