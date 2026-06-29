22c411b9e7f2d8c3532e79197638c510a84f211c
Baesw <hahabsw@gmail.com>
2026-06-29 19:07:07 +0900

    Update hosted demo to NexPress 0.3.25 (#12)
    * Update NexPress demo to 0.3.25
    
    * Add temporary demo migration endpoint
    
    * Remove temporary demo migration endpoint

---
+++ drizzle/0002_nebulous_bulldozer.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "np_c_discussions" ADD COLUMN "published_at" timestamp with time zone;--> statement-breakpoint
+ALTER TABLE "np_c_pages" ADD COLUMN "published_at" timestamp with time zone;
