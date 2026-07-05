9b0abbcb1358cec32c4ca6b9b11f7a50fdcdb2a2
Baesw <hahabsw@gmail.com>
2026-07-05 17:19:08 +0900

    fix: trace sharp native packages on Vercel (#15)

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,11 @@
 import { createNextConfig } from "@nexpress/app/config/next-config";
 
-export default createNextConfig();
+export default createNextConfig({
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/.pnpm/sharp@0.35.3*/node_modules/sharp/**/*",
+      "./node_modules/.pnpm/@img+sharp-linux-x64@0.35.3/node_modules/@img/sharp-linux-x64/**/*",
+      "./node_modules/.pnpm/@img+sharp-libvips-linux-x64@1.3.2/node_modules/@img/sharp-libvips-linux-x64/**/*",
+    ],
+  },
+});
